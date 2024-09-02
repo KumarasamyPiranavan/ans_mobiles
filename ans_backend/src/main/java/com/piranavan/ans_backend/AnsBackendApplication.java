8c2d11da9373959a037e272244acd0375c691a0f
KumarasamyPiranavan <kumarasamy.piranavan@gmail.com>
2024-09-02 14:41:56 +0530

    first commit

---
+++ ans_backend/src/main/java/com/piranavan/ans_backend/AnsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piranavan.ans_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsBackendApplication.class, args);
+	}
+
+}
---
+++ ans_backend/src/test/java/com/piranavan/ans_backend/AnsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piranavan.ans_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
